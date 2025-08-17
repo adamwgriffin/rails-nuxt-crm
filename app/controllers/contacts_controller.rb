e1a81fc2c4404977cd9d9b5aa2d91a896a3b943c
Adam Griffin <ag@agriffin.me>
2025-08-17 14:11:14 -0700

    Removed comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,14 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: %i[ show update destroy ]
 
-  # GET /contacts
   def index
     @contacts = Contact.all
   end
 
-  # GET /contacts/1
   def show
     render json: @contact
   end
 
-  # POST /contacts
   def create
     @contact = Contact.new(contact_params)
 
@@ -22,7 +19,6 @@ class ContactsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /contacts/1
   def update
     if @contact.update(contact_params)
       render json: @contact
@@ -31,18 +27,15 @@ class ContactsController < ApplicationController
     end
   end
 
-  # DELETE /contacts/1
   def destroy
     @contact.destroy!
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_contact
       @contact = Contact.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def contact_params
       params.expect(contact: [ :first_name, :middle_name, :last_name, :birthday, :notes ])
     end
